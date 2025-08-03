460d5c9e6aac998ffec7e3f12efad167e3472c2d
ElChinoGamer <62371760+ElChinoGamer@users.noreply.github.com>
2025-08-02 22:28:20 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,15 @@
-// firebase-config.js (versión compat, sin "import")
-
+// Configuración de Firebase (compatibilidad con HTML puro)
 const firebaseConfig = {
   apiKey: "AIzaSyAHqZs-JDn86AY8NZ2mpUY_4n3-TfaQgVI",
   authDomain: "papeleriaf-9f0e2.firebaseapp.com",
   projectId: "papeleriaf-9f0e2",
-  storageBucket: "papeleriaf-9f0e2.firebasestorage.app",
+  storageBucket: "papeleriaf-9f0e2.appspot.com",
   messagingSenderId: "654877673833",
   appId: "1:654877673833:web:101679a22ec915f50a95f8"
 };
 
-// Inicializar Firebase y Firestore
+// Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
+
+// Inicializar Firestore
 const db = firebase.firestore();
